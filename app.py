b200057353c7a161e6aaf88c44b0a3f8d42bd5d0
Varun_Bollam <varun29star@gmail.com>
2022-09-18 01:03:41 +0530

    updated

---
+++ app.py
@@ -0,0 +1,28 @@
+import pickle
+from flask import Flask, request, app, jsonify, url_for, render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+#Loading model in VS code
+DTModel=model.pickle.load(open('DTmodel.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.rout('/predit_api',methods=['POST'])
+
+def predit_api():
+    data=request.json['data']
+    print(data)
+    #print(np.array(list(data.values())[0]).reshape(1,-1))
+    output=DTModel.predit(data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__ =="__main__":
+    app.run(debug=True)
+
+
+
